3d1f54b43b3fc64f5aba6974c12b399e96d99261
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2016-10-31 18:53:15 -0600

    Changed the name of the directory

--- python_code/code/linked_list.py
+++ python_code/code/linked_list.py
@@ -1,12 +1,14 @@
 class Node:
+    # Class constructor
     def __init__(self, data):
         self.data = data
         data.linked_list_node = self 
         self.next = None
         self.prev = None
 
+    # A string representation of the node
     def __str__(self):
-        return str(self.data)
+        return "(" + str(self.data) + ")"
 
 class LinkedList:
     # Class constructor
@@ -20,6 +22,7 @@ class LinkedList:
         return self.size;
 
     # iterator over linked list
+    # used for the for loops (e.g. for node in nodes.iter())
     def iter(self):
         if (not self.empty()):
             curr_node = self.head
@@ -61,9 +64,21 @@ class LinkedList:
             self.tail = node
             self.size += 1
 
-    # insert element at the position pos
-    def insertAtPos(self, node, pos):
-        pass
+    # Return node at position pos
+    def node_at(self, pos):
+        if (self.empty()):
+            return None
+
+        if (pos > self.size):
+            pos = self.size - 1
+        elif (pos < 0):
+            pos = 0
+
+        node = self.head    
+        for i in range(pos):
+            node = node.next
+
+        return node
 
     # insert element after a specific element
     def insertAfterNode(self, new_node, old_node):
@@ -73,14 +88,28 @@ class LinkedList:
     def insertBeforeNode(self, new_node, old_node):
         pass
 
+    # insert element at the position pos
+    def insertAtPos(self, node, pos):
+        pass
+
     # deletes element at posisiton pos
     def deleteAtPos(self, pos):
-        pass
+        self.delete(self.node_at(pos))
 
     # delete element
     def delete(self, node):
+        if (node is None):
+            return
+
         if (self.size == 1):
             self.clear();
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            if (node == self.tail):
+                self.tail = node.prev
+            if (node == self.head):
+                self.head = node.next
 
     # delete all the elements from the list
     def clear(self):
